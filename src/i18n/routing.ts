47e29793ba9848516ae0127e6fef57ab8bc9b33e
bkantorovich <bkantreg@gmail.com>
2024-12-11 05:35:57 +0400

    redirect fix

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,7 +3,7 @@ import { createSharedPathnamesNavigation } from 'next-intl/navigation';
 export const routing = {
   locales: ['en', 'ru'],
   defaultLocale: 'ru' as const,
-  localePrefix: 'always' as const
+  localePrefix: 'as-needed' as const
 };
 
 export const { Link, redirect, usePathname, useRouter } = createSharedPathnamesNavigation(routing); 
